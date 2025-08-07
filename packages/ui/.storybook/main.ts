e936ad635d8c100d2354f117e512b1db8cb405f7
Stefan Huber <mail@stefanhuber.at>
2025-08-07 22:06:21 +0200

    fix: swiper import

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,10 +1,17 @@
+import { createRequire } from "node:module";
+import { dirname, join } from "node:path";
+const require = createRequire(import.meta.url);
 const config = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [],
   framework: {
-    name: "@stencil/storybook-plugin"
+    name: getAbsolutePath("@stencil/storybook-plugin")
   },
   staticDirs: ["./public"],
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
